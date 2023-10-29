d554770cf518a1d2cdfef3f98acadd9ab661495d
abdou6666 <abdousfayhi12@gmail.com>
2023-10-29 22:14:41 +0100

    changes in owl file

--- src/main/java/com/example/demo/controller/Test.java
+++ src/main/java/com/example/demo/controller/Test.java
@@ -9,11 +9,8 @@ import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.tools.JenaEngine;
 
@@ -26,7 +23,7 @@ import com.example.demo.tools.JenaEngine;
 
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(path = "/api",produces = "application/json")
 @CrossOrigin(origins = "http://127.0.0.1:5500")
 public class Test {
     @GetMapping("/admins")
@@ -68,13 +65,14 @@ public class Test {
         String qexec = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
                 "PREFIX project: <http://www.semanticweb.org/anis/ontologies/2023/9/projet#>\n" +
                 "\n" +
-                "SELECT ?email ?mot_de_passe ?nom\n" +
+              /*      "SELECT *\n" +
                 "WHERE {\n" +
                 "  ?admin rdf:type project:Admin .\n" +
                 "  ?admin project:Email ?email .\n" +
                 "  ?admin project:Mot_de_passe ?mot_de_passe .\n" +
                 "  ?admin project:Nom ?nom .\n" +
-                "}\n";
+                "}\n"; */
+                "select?entité\n";
 
         Model model = JenaEngine.readModel("data/sem.owl");
 
@@ -97,6 +95,39 @@ public class Test {
 
         return j.getJSONObject("results").getJSONArray("bindings").toString();
     }
+    @GetMapping("/getClientAndTheirOrders")
+    public String getClientAndTheirOrders() {
+        String qexec = "PREFIX projet: <http://www.semanticweb.org/anis/ontologies/2023/9/projet#>\n" +
+                "\n" +
+                "# Query to retrieve the names of admins and their associated to-do lists\n" +
+                "# Query to retrieve all products with their associated categories\n" +
+               "SELECT ?description\n" +
+                "WHERE {\n" +
+                "  ?client a projet:Client ;\n" +
+                "          projet:estReclamer ?produit .\n" +
+                "  ?produit projet:Description ?description .\n" +
+                "}";
 
+        Model model = JenaEngine.readModel("data/sem.owl");
+
+        QueryExecution qe = QueryExecutionFactory.create(qexec, model);
+        ResultSet results = qe.execSelect();
+
+        // write to a ByteArrayOutputStream
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        ResultSetFormatter.outputAsJSON(outputStream, results);
+
+        // and turn that into a String
+        String json = new String(outputStream.toByteArray());
+
+        JSONObject j = new JSONObject(json);
+        System.out.println(j.getJSONObject("results").getJSONArray("bindings"));
+
+        JSONArray res = j.getJSONObject("results").getJSONArray("bindings");
+
+
+        return j.getJSONObject("results").getJSONArray("bindings").toString();
+    }
 
 }
