670c7bd7695deff31a95c5f0946cd58c982697c0
brahimABD98 <brahim.abderrahim@esprit.tn>
2023-10-30 12:19:11 +0100

    fix

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,82 +1,79 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import './App.css';
 import axios from 'axios';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import DisplayAbdouReclamation from './components/DisplayAbdouReclamation';
 import DisplayAbdouReclamationWithResponses from './components/DisplayAbdouReclamationWithResponses';
-import DisplayAllReclamationsAndResponsesWithClientAndAdminData
-    from './components/DisplayAllReclamationsAndResponsesWithClientAndAdminData';
+import DisplayAllReclamationsAndResponsesWithClientAndAdminData from './components/DisplayAllReclamationsAndResponsesWithClientAndAdminData';
 import ShowSearchResult from './components/ShowSearchResult';
-import DisplayAllOrdersAndPayements from "./components/DisplayAllOrdersAndPayements.jsx";
-
+import Displayproduct from './components/product/Displayproduct'
 const base_url = 'http://localhost:8005/api'
-
 function App() {
     const [allReclamation, setAllReclamation] = useState([]);
+
     const [displayAllReclamation, setDisplayAllReclamation] = useState(false)
 
-    const [allReclamationsAndResponses, setAllReclamationAndResponses] = useState([])
+    const [allProduct, setAllProduct] = useState([]);
+    const [displayAllProduct, setDisplayAllProduct] = useState(false)
+
+
+    const [allReclamationsAndResponses,setAllReclamationAndResponses] = useState([])
     const [displayReclamationAndResponses, setDisplayReclamationAndResponses] = useState(false)
 
-    const [allReclamationsAndResponsesWithClientAndAdminData, setAllReclamationsAndResponsesWithClientAndAdminData] = useState([])
+    const [allReclamationsAndResponsesWithClientAndAdminData,setAllReclamationsAndResponsesWithClientAndAdminData] = useState([])
     const [displayAllReclamationsAndResponsesWithClientAndAdminData, setDisplayAllReclamationsAndResponsesWithClientAndAdminData] = useState(false)
 
-    const [allOrderAndPayements, setAllOrderAndPayements] = useState([]);
-    const [displayAllOrderAndPayements, setDisplayAllOrderAndPayements] = useState(false)
 
     const [searchReclamationInput, setSearchReclamationInput] = useState('')
-    const [searchReclamationResult, setSearchReclamationResult] = useState([])
-    const [showResult, setShowResult] = useState(false)
+    const [searchReclamationResult,setSearchReclamationResult] = useState([])
+    const [showResult,setShowResult] = useState(false)
 
 
     async function fetchAllReclamations() {
-        const {data} = await axios.get(`${base_url}/abdou/reclamations`)
+        const { data } = await axios.get(`${base_url}/abdou/reclamations`)
         setAllReclamation(data)
     }
-
+    async function fetchAllProduct() {
+        const { data } = await axios.get(`${base_url}/product`)
+        setAllProduct(data)
+    }
     async function fetchReclamationsAndResponses() {
-        const {data} = await axios.get(`${base_url}/abdou/getReclamationsAndResponses`)
+        const { data } = await axios.get(`${base_url}/abdou/getReclamationsAndResponses`)
         setAllReclamationAndResponses(data)
     }
-
-    async function fetchOrderAndPayements() {
-        const {data} = await axios.get(`${base_url}/GetCommandesMontantProduit`)
-        setAllOrderAndPayements(data)
-
-    }
-
     async function fetchClientsReclamationsWithClienDataAndAdminData() {
-        const {data} = await axios.get(`${base_url}/abdou/getClientsReclamationsWithClienDataAndAdminData`)
+        const { data } = await axios.get(`${base_url}/abdou/getClientsReclamationsWithClienDataAndAdminData`)
         setAllReclamationsAndResponsesWithClientAndAdminData(data)
     }
 
     async function searchReclamation() {
-        const {data} = await axios.get(`${base_url}/abdou/searchReclamation/${searchReclamationInput}`)
+        const { data } = await axios.get(`${base_url}/abdou/searchReclamation/${searchReclamationInput}`)
         setSearchReclamationResult(data)
         setShowResult(prev => !prev)
 
     }
 
     useEffect(() => {
+        fetchAllProduct()
         fetchAllReclamations()
         fetchReclamationsAndResponses()
         fetchClientsReclamationsWithClienDataAndAdminData()
-        fetchOrderAndPayements()
-    }, [])
+    },[])
 
     return (
         <div>
             <div>
-                <button onClick={() => setDisplayAllReclamation(prev => !prev)}>Display All Reclamations</button>
+                <button onClick={() => setDisplayAllProduct(prev => !prev)}>Display All Product </button>
+                {allProduct && displayAllProduct && <Displayproduct allProduct={allProduct}/>}
+            </div>
+            <div>
+                <button onClick={() => setDisplayAllReclamation(prev => !prev)}>Display All Reclamations </button>
                 {allReclamation && displayAllReclamation && <DisplayAbdouReclamation allReclamation={allReclamation}/>}
             </div>
 
             <div>
-                <button onClick={() => setDisplayReclamationAndResponses(prev => !prev)}>Display All Reclamations with
-                    responses
-                </button>
-                {allReclamationsAndResponses && displayReclamationAndResponses &&
-                    <DisplayAbdouReclamationWithResponses allReclamationsAndResponses={allReclamationsAndResponses}/>}
+                <button onClick={() => setDisplayReclamationAndResponses(prev => !prev)}>Display All Reclamations with responses </button>
+                {allReclamationsAndResponses && displayReclamationAndResponses && <DisplayAbdouReclamationWithResponses allReclamationsAndResponses={allReclamationsAndResponses}/>}
             </div>
 
             <div>
@@ -84,22 +81,13 @@ function App() {
                     Display All Reclamations with responses with client and admin data
                 </button>
                 {allReclamationsAndResponsesWithClientAndAdminData && displayAllReclamationsAndResponsesWithClientAndAdminData &&
-                    <DisplayAllReclamationsAndResponsesWithClientAndAdminData
-                        allReclamationsAndResponsesWithClientAndAdminData={allReclamationsAndResponsesWithClientAndAdminData}/>}
+                    <DisplayAllReclamationsAndResponsesWithClientAndAdminData allReclamationsAndResponsesWithClientAndAdminData={allReclamationsAndResponsesWithClientAndAdminData} />}
             </div>
 
             <div>
-                <input type="text" placeholder='search for reclamation' value={searchReclamationInput}
-                       onChange={(e) => setSearchReclamationInput(e.target.value)}/>
+                <input type="text" placeholder='search for reclamation' value={searchReclamationInput} onChange={(e) => setSearchReclamationInput(e.target.value)} />
                 <button onClick={searchReclamation}>Search</button>
-                {showResult && searchReclamationResult &&
-                    <ShowSearchResult searchReclamationResult={searchReclamationResult}/>}
-            </div>
-            <div>
-                <button onClick={()=>setDisplayAllOrderAndPayements(prev=>!prev)}>Display All Order And Payements</button>
-                {allOrderAndPayements && displayAllOrderAndPayements &&
-                    <DisplayAllOrdersAndPayements allOrderAndPayements={allOrderAndPayements}/>}
-
+                {showResult && searchReclamationResult && <ShowSearchResult searchReclamationResult={searchReclamationResult}/> }
             </div>
         </div>
     )
